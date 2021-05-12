edfbb3a95f2200f90b239bcf3ad8d9be2c115749
nivek96 <kevinduliepre@gmail.com>
2021-05-12 11:41:00 -0400

    configuring an auth-reducer file

---
+++ src/store/auth-context.js
@@ -0,0 +1,43 @@
+import { createContext, useState, useEffect } from "react";
+
+const AuthContext = createContext({
+  isLoading: false,
+  onlogin: () => {},
+  onLogout: (email, password) => {},
+});
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
